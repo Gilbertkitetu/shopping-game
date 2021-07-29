b9f3fe6e2967977048565a0eb2b3f8bcde8c37d8
Gilbertkitetu <gilbertkitetu2@gmail.com>
2021-07-29 15:01:32 +0300

    list of boxes in the mainbusket

--- src/components/busket.js
+++ src/components/busket.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
+import './styles/busket.css';
 //imports
 import BusketBox from './busketBox';
 
 function Busket (){
 
+  const numberOfBoxes = [1,2,3,4,5,6,7,8,9,10,11,12];
+  const arrayOfBoxes = numberOfBoxes.map((box) => 
+  <li className="busket-item">{box}</li>
+  );
+
 
     return(
-        <div className="busket">
-            <BusketBox />
-        </div>
+       
+
+          <ul>{arrayOfBoxes}</ul>
+            
+     
     );
 }
+
+//const boxes = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 export default Busket;
