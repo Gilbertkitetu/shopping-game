a779ba11570da5aae8dc0dade59d70dbf78a90f9
Gilbertkitetu <gilbertkitetu2@gmail.com>
2021-08-02 15:58:56 +0300

    149914991499 solved

--- src/components/fruit-images.js
+++ src/components/fruit-images.js
@@ -1,51 +1,30 @@
 import React from 'react';
 
 
-import i1 from '../images/fruits/i1.jpg';
-import i2 from '../images/fruits/i2.jpg';
-import i3 from '../images/fruits/i3.jpg';
-import i4 from '../images/fruits/i4.jpg';
-import i5 from '../images/fruits/i5.jpg';
-import i6 from '../images/fruits/i6.jpg';
-import i7 from '../images/fruits/i7.jpg';
-import i8 from '../images/fruits/i8.jpg';
-import i9 from '../images/fruits/i9.jpg';
-import i10 from '../images/fruits/i10.jpg';
-import i11 from '../images/fruits/i11.jpg';
-import i12 from '../images/fruits/i12.jpg';
-import i13 from '../images/fruits/i13.jpg';
-import i14 from '../images/fruits/i14.jpg';
-import i15 from '../images/fruits/i15.jpg';
-import i16 from '../images/fruits/i16.jpg';
-import i17 from '../images/fruits/i17.jpg';
-import i18 from '../images/fruits/i18.jpg';
-import i19 from '../images/fruits/i19.jpg';
-import i20 from '../images/fruits/i20.jpg';
-
-
 
 const fruits = [
-    {image: i1},
-    {image: i2},
-    {image: i3},
-    {image: i4},
-    {image: i5},
-    {image: i6},
-    {image: i7},
-    {image: i8},
-    {image: i9},
-    {image: i10},
-    {image: i11},
-    {image: i12},
-    {image: i13},
-    {image: i14},
-    {image: i15},
-    {image: i16},
-    {image: i17},
-    {image: i18},
-    {image: i19},
-    {image: i20}
-]
+    {id: 1, src : '../images/fruits/i1.jpg'},
+    {id: 2, src : '../images/fruits/i2.jpg'},
+    {id: 3, src : '../images/fruits/i3.jpg'},
+    {id: 4, src : '../images/fruits/i4.jpg'},
+    {id: 5, src : '../images/fruits/i5.jpg'},
+    {id: 6, src : '../images/fruits/i6.jpg'},
+    {id: 7, src : '../images/fruits/i7.jpg'},
+    {id: 8, src : '../images/fruits/i8.jpg'},
+    {id: 9, src : '../images/fruits/i9.jpg'},
+    {id: 10, src : '../images/fruits/i10.jpg'},
+    {id: 11, src : '../images/fruits/i11.jpg'},
+    {id: 12, src : '../images/fruits/i12.jpg'},
+    {id: 13, src : '../images/fruits/i13.jpg'},
+    {id: 14, src : '../images/fruits/i14.jpg'},
+    {id: 15, src : '../images/fruits/i15.jpg'},
+    {id: 16, src : '../images/fruits/i16.jpg'},
+    {id: 17, src : '../images/fruits/i17.jpg'},
+    {id: 18, src : '../images/fruits/i18.jpg'},
+    {id: 19, src : '../images/fruits/i19.jpg'},
+    {id: 20, src : '../images/fruits/i20.jpg'},
+
+];
 
 export default fruits;
 
